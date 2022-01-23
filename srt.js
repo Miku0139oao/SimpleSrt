1d272915ee7bf879484c22211ef5874f49aff75f
Miku0139 <62833794+Miku0139oao@users.noreply.github.com>
2022-01-23 23:21:45 +0800

    Fix Time Calculation

--- srt.js
+++ srt.js
@@ -15,11 +15,13 @@ Decode()
 
 async function Decode(){
     let content = ""
-    console.log(`SimpleSrt:\n 2021/07/19 \n Developer: Miku0139#7414 \n 版權所有,抄襲必究 \n 文字格式: 00:01 內容`)
+    let CalNum = 0
+    // console.log(`SimpleSrt:\n 2021/07/19 \n Developer: Miku0139#7414 \n 版權所有,抄襲必究 \n 文字格式: 00:01 內容`)
     for (let i = 0; i <= RawData.length-1 ; i++) {
         let TimeRegex = new RegExp(/^([\w]+):([\w]+) ([\s\S]+)/gm).exec(RawData[i])
+
         if(TimeRegex) {
-            let CalNum = i + 1
+           CalNum = i + 1
             if(CalNum > RawData.length-1){
                 CalNum = RawData.length-1
             }
@@ -27,19 +29,38 @@ async function Decode(){
             let Next = await NextLine(RawData[CalNum]);
             if(Next){
                 if(i !== RawData.length -1) {
+                    let split = TimeRegex[3].toString().split("\\n")
+
+                    if (split.length > 1) {
+                        for (let j = 0; j < split.length; j++) {
+                            TimeRegex[3] = split[0] + "\n" + split[j]
+                        }
+                    }
+                    console.log(split[0],split[0].length)
+
                     content = content + `${i}\n00:${TimeRegex[1]}:${TimeRegex[2]},${LastTime} --> ${Next.text},${Next.time}\n${TimeRegex[3]}\n\n`;
+
                 }else {
                     let fil = `0${parseInt(TimeRegex[2])+1}`
                     if(parseInt(fil) > 10){
                         fil = parseInt(TimeRegex[2])+1
                     }
-                    content = content + `${i}\n00:${TimeRegex[1]}:${TimeRegex[2]},${LastTime} --> 00:${TimeRegex[1]}:${fil},${LastTime+100}\n${TimeRegex[3]}\n\n`;
+                    let split = TimeRegex[3].toString().split("\\n")
+                    if (split.length > 1) {
+                        for (let j = 0; j < split.length; j++) {
+                            TimeRegex[3] = split[0] + "\n" +  split[j]
+                        }
+                    }
+                    console.log(split[0],split[0].length)
+
+                    content = content + `${i}\n00:${TimeRegex[1]}:${TimeRegex[2]},${LastTime} --> ${Next.text},${Next.time}\n${TimeRegex[3]}\n\n`;
+                    console.log(content)
                 }
             }
             //console.log(RawData[i])
         }
     }
-    console.log(`已成功運算 ${RawData.length}行字幕`)
+    console.log(`已成功運算 ${RawData.length} 行字幕`)
     //console.log(content)
     await fs.writeFileSync("./Output.srt",content,"utf-8")
 
@@ -47,24 +68,28 @@ async function Decode(){
 
 async function NextLine(data){
     let StringDec = new RegExp(/^([\w]+):([\w]+) ([\s\S]+)/gm).exec(data)
+
     if(StringDec){
 
         time += 120
 
-        if(time === LastTime){
+        if(time != 0){
+            LastTime = time
         }
 
-        if(time !== 0){
-            LastTime = time
+        if(time < LastTime){
+            time = LastTime+100
+        }
+
+
+        if(LastTime > 800){
+            LastTime = time + 120
         }
 
         if(time > 800){
             time = 130
         }
 
-        if(LastTime > 800){
-            LastTime = time + 120
-        }
 
 
         const result = {
